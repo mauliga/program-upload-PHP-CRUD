61a38d12e2c0f456b8bfeacef8c2c29f05389d0c
MauligaPenyejuknate <mauliga34@gmail.com>
2021-10-04 02:05:16 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "mynotescode";
-
-$pdo = new PDO('mysql:host='. $host.';dbname='. $database, $username, $password);
-?>
